786c3521dab6b0605ca2c194d14af49652a1aab1
Reese M Bowman <reesemb1@gmail.com>
2020-01-02 22:35:13 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,7 +17,8 @@ def reduce(source_array, starting_point = nil)
     index = 1 
   end
   while index < source_array.length do
-    total = yield(total, source_array[index])
+    total += yield(source_array[index])
+    #total = yield(total, source_array[index])
     index += 1 
   end
   total
