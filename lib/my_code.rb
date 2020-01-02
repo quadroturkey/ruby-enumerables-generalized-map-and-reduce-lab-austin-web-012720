a1570352a2c7530e2a47859035c8061ac1884a5c
Reese M Bowman <reesemb1@gmail.com>
2020-01-02 22:33:41 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(source_array)
+  index = 0 
+  result = []
+  while index < source_array.length do
+    result << yield(source_array[index])
+    index += 1 
+  end
+  result
+end
+
+def reduce(source_array, starting_point = nil)
+  if starting_point
+    total = starting_point
+    index = 0 
+  else
+    total = source_array[0]
+    index = 1 
+  end
+  while index < source_array.length do
+    total = yield(total, source_array[index])
+    index += 1 
+  end
+  total
+end
